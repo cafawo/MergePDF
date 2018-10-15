f3c72f8c494063c79b7335493ce7567ec01b11bf
cafawo <fabian.woebbeking@gmail.com>
2018-10-15 09:29:31 +0200

    Added tkinter.filedialog

--- mergePDF.py
+++ mergePDF.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 
 import os
+import sys
 from PyPDF2 import PdfFileMerger
-    
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+from tkinter import messagebox
+
 def mergePDF(input_folder = True, output_file = True, sort = True):
-    local_dir = os.path.dirname(__file__)
-    if input_folder: input_folder = os.path.join(local_dir, 'input')
+    Tk().withdraw() # keep the root window from appearing
+    local_dir = askdirectory() # show an "Open" dialog box and return the path to the selected file
+    if not local_dir: sys.exit()
+    print('Directory: ' + str(local_dir))
+
+    if input_folder: input_folder = local_dir
     if output_file: output_file = os.path.join(local_dir, 'mergePDF_output.pdf')
     # input_folder can be manually assigned
     input_files = os.listdir(input_folder)
@@ -18,7 +26,10 @@ def mergePDF(input_folder = True, output_file = True, sort = True):
         merger.append(os.path.join(input_folder,pdf))
     merger.write(output_file)
 
-    print(input_files)
+    print('Files merged: '+str(input_files))
+    print('Output file: '+str(output_file))
+    
+    messagebox.showinfo("mergePDF", str(len(input_files))+' PDF files merged into: '+output_file)
     
 if __name__ == '__main__':
     mergePDF()
